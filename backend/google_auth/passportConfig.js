6bbd7938809af7835e6f0cbb48eafa5f515f4eec
Sk8Mahammad-Irfan9 <lazybum729@gmail.com>
2025-04-13 19:03:28 +0530

    google callback url

--- backend/google_auth/passportConfig.js
+++ backend/google_auth/passportConfig.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/api/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await userModel.findOne({ googleId: profile.id });
