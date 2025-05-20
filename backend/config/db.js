b978691a8fd928819b8dff3f15324c561a14242b
Sk8Mahammad-Irfan9 <lazybum729@gmail.com>
2025-05-20 10:38:05 +0530

    changes in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       serverSelectionTimeoutMS: 30000, // Keep trying to send operations for 30 seconds
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("Mongo db connected");
   } catch (err) {
